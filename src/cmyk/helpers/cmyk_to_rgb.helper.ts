d16d388982e6590a980836909776288a1c2c7874
fwmakc <fwmakcpro@yandex.ru>
2025-11-25 16:54:19 +0300

    modified:   src/cmyk/helpers/cmyk_to_rgb.helper.ts

--- src/cmyk/helpers/cmyk_to_rgb.helper.ts
+++ src/cmyk/helpers/cmyk_to_rgb.helper.ts
@@ -4,19 +4,14 @@ import type { ICMYK } from '../cmyk.interface';
 export function cmykToRgb(cmyk: ICMYK): IRGB {
   let { c, m, y, k, a } = cmyk;
 
-  c = c / 100;
-  m = m / 100;
-  y = y / 100;
-  k = k / 100;
+  c /= 100;
+  m /= 100;
+  y /= 100;
+  k /= 100;
 
-  const r = 255 * (1 - c) * (1 - k);
-  const g = 255 * (1 - m) * (1 - k);
-  const b = 255 * (1 - y) * (1 - k);
+  const r = Math.round(255 * (1 - c) * (1 - k));
+  const g = Math.round(255 * (1 - m) * (1 - k));
+  const b = Math.round(255 * (1 - y) * (1 - k));
 
-  return {
-    r: Math.round(r),
-    g: Math.round(g),
-    b: Math.round(b),
-    a,
-  };
+  return { r, g, b, a };
 }
